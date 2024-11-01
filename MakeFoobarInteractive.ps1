e03c69f3c5101d2115297bcd72dddfe793500901
Maarten van Bunnik <maarten.van.bunnik@interstellar.nl>
2024-11-01 16:01:08 +0100

    v1

---
+++ MakeFoobarInteractive.ps1
@@ -0,0 +1,9 @@
+﻿$processName = "foobar2000"
+$foobar2000exe = "C:\Program Files (x86)\foobar2000\foobar2000.exe"
+
+$process = Get-Process -Name $processName | Where-Object {$_.SessionId -eq 0} #check if process is not running interactive
+
+if ($process){
+    Stop-Process -Name $processName -Force
+    Start-Process -FilePath $foobar2000exe
+}
